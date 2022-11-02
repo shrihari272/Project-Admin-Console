735661dbe9cc72ad7f877af8504d3f8f15295ef9
KIRA <80241966+shrihari272@users.noreply.github.com>
2022-11-02 18:32:06 +0530

    Heroku CLI

---
+++ controllers/dbUsers.js
@@ -0,0 +1,35 @@
+const users =
+    [
+        {
+            "name": "Shrihari",
+            "email": "admin",
+            "pass": "$2b$10$xQ49ST/NBLHkpYOy51MQEuK0K3lIgKc5CQyeLjSQrha60INWVAPdu"
+        },
+        {
+            "name":"Ashik",
+            "email":"ashik@123",
+            "pass":"$2b$10$qFy30IC4pQsg.ezVYfxNLOcIPzKdSh7seB5qymLs/dEgEKRoRYzx2"
+        },
+        {
+            "name":"Shashank",
+            "email":"SSaralaya",
+            "pass":"$2b$10$j8hr39gk5F7RnUpnCpJElOYuawsF35QFrcCuGx6FAVBOhxgprbJnu"
+        },
+        {
+            "name":"Dayananda",
+            "email":"daya bnr",
+            "pass":"$2b$10$hECr5IcIuC1wRCxRSwjTtODx2zZWuxl6Th6sptjjGXdMjQCMfsmli"
+        },
+        {
+            "name":"Vivian Serrao",
+            "email":"vvn",
+            "pass":"$2b$10$Cyjfr8XflAA/9mp521o3EOeMRtdYPQom.QHTPp3gR35I0htl5Lv3q"
+        },
+        {
+            "name":"PRAJWAL",
+            "email":"PRAJWAL",
+            "pass":"$2b$10$1w44iM9AjPl2QC5ll9x79..PhXXqEkmVkKrnaGIkPh5A8qM7OPuGe"
+        }
+    ]
+
+module.exports = users
